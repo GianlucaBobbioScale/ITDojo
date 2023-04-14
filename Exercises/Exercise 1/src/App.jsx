c9794de739e5719ef4d1669161e3329500ab6b64
GianlucaBobbio <109114462+GianlucaBobbioScale@users.noreply.github.com>
2023-04-14 01:24:26 -0300

    Add files via upload

---
+++ Exercises/Exercise 1/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import BadTaskList from './BadTaskList';
+
+const darkTheme = createTheme({
+  palette: {
+    type: 'dark',
+  },
+});
+
+export function App(props) {
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <BadTaskList />
+    </ThemeProvider>
+  );
+}
